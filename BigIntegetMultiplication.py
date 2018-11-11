d849116ed95a20b39dfdf88d882d25b11ae2b8ff
zijiehan <306631986@qq.com>
2018-11-11 21:07:25 +0800

    超大整数加减乘法运算，含测试数据以及注释。

--- BigIntegetMultiplication.py
+++ BigIntegetMultiplication.py
@@ -9,6 +9,8 @@ import math
 class BigInteger(object):
     @staticmethod
     def Multiplicate(a , b):
+        # 封装乘法函数
+        
         ans = BigInteger.mMultiplicate(a , b)
         while ans[0] == "0":
             ans = ans[1:len(ans)]
@@ -16,11 +18,18 @@ class BigInteger(object):
 
     @staticmethod
     def mMultiplicate(a, b):
+        """
+            未封装的乘法函数，它可能让最高位的返回值是 0 ，因为在乘法运算的过程中需要考虑字符串的长度对齐
+        :param a: a str of first number
+        :param b: a str of second number
+        :return: a str of the result
+        """
         if b == "0":
             return "0"
         if b == "1":
             return a
         if len(a) <= 4 and len(b) <= 4:
+            # 进行计算
             a_ = int(a)
             b_ = int(b)
             m = a_ * b_
@@ -39,20 +48,16 @@ class BigInteger(object):
             c_ = b[:len (b) - n]
             d_ = b[-n:]
 
+
+        # 分治 - Karatsuba算法
+
         z2 = BigInteger.mMultiplicate(a_,c_)
-        ans_ = int(a_)*int(c_)
         z0 = BigInteger.mMultiplicate(b_,d_)
-        ans1 = int(b_)*int(d_)
         a1 = BigInteger.Add(a_,b_)
-        ans1_ = int(a_)+int(b_)
         a2 = BigInteger.Add(c_,d_)
-        ans1__ = int(c_)+int(d_)
         z1_a = BigInteger.mMultiplicate(a1,a2)
-        ans2 = int(a1)*int(a2)
         z1_b = BigInteger.Add(z2,z0)
-        ans3 = int(z2)+int(z0)
         z1 = BigInteger.Minus(z1_a,z1_b)
-        ans4 = int(z1_a)-int(z1_b)
 
         i = 0
         while i < n:
@@ -67,6 +72,15 @@ class BigInteger(object):
 
     @staticmethod
     def Minus(a,b):
+        """
+            这是一个封装好的减法函数，它可以为减法分类，并对两个数字进行初步的处理：
+        1. 比较两个数字的大小，总让大的减去小的。
+        2. 在字符串长度相等的情况下，将两个数字高位相同的直接消去
+
+        :param a: a str of the minuend
+        :param b: a str of the subtractor
+        :return: a str of the result
+        """
         if a[0] == "-" and b[0] != "-":
             return "-"+BigInteger.Add(b,a[1:len(a)])
         if a[0] != "-" and b[0] == "-":
@@ -104,9 +118,20 @@ class BigInteger(object):
 
     @staticmethod
     def mMinus( a , b ):
+        """
+            这是一个没有进行封装的减法函数，必须满足这个条件的条件：
+            must satisfy: a > b
+
+        :param a: a str of the minuend
+        :param b: a str of the subtractor
+        :return: a result(str} and a flag(a number in (1,-1))
+                when flag = -1: borrow
+        """
         if len(b) == 0:
             return a,1
         if len(a) <= 4 and len(b) <= 4:
+            # 长度小于4时直接进行计算，可以提高效率，注意需要对齐字符串长度
+
             m = int(a) - int(b)
             if m < 0:
                 flagNum = int(math.pow (10 , len(a)))
@@ -140,11 +165,11 @@ class BigInteger(object):
         d_ = b[-n:]
 
         low,low_flag = BigInteger.mMinus(b_,d_)
-        anslow = int(b_) - int(d_)
         high,high_flag = BigInteger.mMinus(a_,c_)
-        anshigh = int(a_) - int(c_)
 
         if low_flag == -1 and high_flag != 0:
+            # 处理借位：可参考 mAdd 函数中的详细注释
+
             newHigh = ""
             i = -1
             while high[i] == "0":
@@ -164,6 +189,12 @@ class BigInteger(object):
 
     @staticmethod
     def Add(a , b):
+        """
+            这是一个用于封装 mAdd 的函数，将加法根据值的不同，分情况讨论。
+        :param a: str of a number
+        :param b: str of a number
+        :return: ans of a + b
+        """
         if a[0] == "-" and b[0] != "-":
             return BigInteger.Minus(b,a[1:len(a)])
         if a[0] != "-" and b[0] == "-":
@@ -175,18 +206,30 @@ class BigInteger(object):
         else:
             ans,flag = BigInteger.mAdd(a , b)
         if flag == 1:
+            # 根据最后是否有进位产生，决定首位是否加 1 。
             ans = "1"+ans
         return ans
 
     @staticmethod
     def mAdd(a , b):
+        """
+            这是一个没有进行封装的加法函数，必须满足这个条件，must satisfy: len(a)>len(b)
+
+        :param a: a str of the first number
+        :param b: a str of the second number
+        :return: a result(str} and a flag(a number in (1,0))
+                when flag = 1:carry-over
+        """
         if len(b) == 0:
             return a,0
         if len(a) <= 4 and len(b) <= 4:
+            # 当子字符串长度小于 4 时，执行加法运算，并根据运算结果，返回结果的字符串和 flag 。注意要让结果和字符串 a 的长度对齐！
+
             m = int(a) + int(b)
             numStr = str(m)
             length = len(numStr)
             if length > len(a):
+                # 字符串对齐
                 numStr = numStr[1:len(numStr)]
                 return numStr,1
 
@@ -209,6 +252,7 @@ class BigInteger(object):
         high,highflag = BigInteger.mAdd(a_,c_)
 
         if lowflag == 1:
+            # 根据高低位的返回值进行进位计算，并向上返回
             i = -1
             newHigh = ""
             while high[i] == "9" and abs(i) < len(high):
